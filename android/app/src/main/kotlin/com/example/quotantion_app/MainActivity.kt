1cff86f964c4245fb4c2b7ae850e9c94be33a400
jotadevss <joaopedropereira3232@hotmail.com>
2024-01-12 10:11:19 -0300

    init commit

---
+++ android/app/src/main/kotlin/com/example/quotantion_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quotantion_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xffF4C92E);
+const Color kBlackColor = Color(0xff2C2C2C);
+const Color kGreenColor = Color(0xff0EAE61);
+const Color kGrayColor = Color(0xff8E8E8E);
+
+const double kBorderRadius = 30;
